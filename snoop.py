9eb7c6be6b0b186fc5460b13c194e709ee3821f9
snooppr <61022210+snooppr@users.noreply.github.com>
2021-02-18 15:03:41 +0300

    upd

--- snoop.py
+++ snoop.py
@@ -45,14 +45,17 @@ _____/ _|  _|\___/ \___/  .__/
                          _|    \033[0m \033[37mv1.2.9\033[34;1m_rus_\033[31;1mSource Demo\033[0m
 """)
 
-print (Fore.CYAN + "#Пример:" + Style.RESET_ALL)
+print (Fore.CYAN + "#Примеры:" + Style.RESET_ALL)
 if sys.platform == 'win32':
     print (Fore.CYAN + " cd с:\snoop" + Style.RESET_ALL)
+    print (Fore.CYAN + " python snoop.py --help" + Style.RESET_ALL, "#справка")
+    print (Fore.CYAN + " python snoop.py username" + Style.RESET_ALL, "#поиск user-a")
+    print (Fore.CYAN + " python snoop.py --module y" + Style.RESET_ALL, "#задействовать плагины")
 else:
     print (Fore.CYAN + " cd ~/snoop" + Style.RESET_ALL)
-print (Fore.CYAN + " python snoop.py --help" + Style.RESET_ALL, "#справка")
-print (Fore.CYAN + " python snoop.py username" + Style.RESET_ALL, "#поиск user-a")
-print (Fore.CYAN + " python snoop.py --module y" + Style.RESET_ALL, "#задействовать плагины")
+    print (Fore.CYAN + " python3 snoop.py --help" + Style.RESET_ALL, "#справка")
+    print (Fore.CYAN + " python3 snoop.py username" + Style.RESET_ALL, "#поиск user-a")
+    print (Fore.CYAN + " python3 snoop.py --module y" + Style.RESET_ALL, "#задействовать плагины")
 console.rule(characters = '=', style="cyan")
 print("")
 
