cebd6d21de400fd0e58b6dd81c348e5efcd75775
snooppr <61022210+snooppr@users.noreply.github.com>
2021-12-11 10:01:28 +0300

    open_br-nettest
    Former-commit-id: d1bebc4fee18714d1b253e72792f4fe768ebff86 [formerly 30a7e182f3e3c7b421e1575257203c02e67962c0] [formerly 0eebc60d142ed94981817941a6a6594c7f144da3 [formerly f331b9128adcbccec5e853ed589cbccc08d927ce]]
    Former-commit-id: 09dd332bfdfcf88626e73450ac91ac45ae50fd93 [formerly d084a578cffae8691a1c508316e1ea6bcac10a7c]
    Former-commit-id: f55d517b6443e70311c50d1b98330cc0fd121320

--- networktest.py
+++ networktest.py
@@ -13,44 +13,46 @@ init(autoreset=True)
 console2 = Console()
 
 def nettest():
-    servers = []
-    threads = None
+    with console2.status("[cyan]Ожидайте, идёт самотестирование сети..."):
+        servers = []
+        threads = None
 
-    s = speedtest.Speedtest()
-    s.get_servers(servers)
-    s.get_best_server()
-    s.download(threads=threads)
-    s.upload(threads=threads)
+        s = speedtest.Speedtest()
+        s.get_servers(servers)
+        s.get_best_server()
+        s.download(threads=threads)
+        s.upload(threads=threads)
 
-    a = s.results.dict()
+        a = s.results.dict()
 
-    d = round(a.get("download")/1000000,2)
-    u = round(a.get("upload")/1000000,2)
-    p = round(a.get("ping"))
-    v4 = a.get("client")
+        d = round(a.get("download")/1000000,2)
+        u = round(a.get("upload")/1000000,2)
+        p = round(a.get("ping"))
+        v4 = a.get("client")
 
-# Скорость загрузки
-    try:
-        if d < 3: d = f"Download: [bold red]{d}[/bold red] Мбит/с"
-        elif 3 <= d <= 5.5: d = f"Download: [yellow]{d}[/yellow] Мбит/с"
-        elif d > 5.5: d = f"Download: [bold green]{d}[/bold green] Мбит/с"
-    except:
-        d = f"Download: [bold red]Сбой[/bold red]"
+# Скорость загрузки.
+        try:
+            if d < 3: d = f"Download: [bold red]{d}[/bold red] Мбит/с"
+            elif 3 <= d <= 5.5: d = f"Download: [yellow]{d}[/yellow] Мбит/с"
+            elif d > 5.5: d = f"Download: [bold green]{d}[/bold green] Мбит/с"
+        except:
+            d = f"Download: [bold red]Сбой[/bold red]"
 
-# Скорость выгрузки
-    try:
-        if u < 0.8: u = f"Upload: [bold red]{u}[/bold red] Мбит/с"
-        elif 0.8 <= u <= 1.5: u = f"Upload: [yellow]{u}[/yellow] Мбит/с"
-        elif u > 1.5: u = f"Upload: [bold green]{u}[/bold green] Мбит/с"
-    except:
-        u = f"Upload: [bold red]Сбой[/bold red]"
-# Ping
-    try:
-        if p >= 250: p = f"Ping: [bold red]{p}[/bold red] мс"
-        elif 60 <= p < 250: p = f"Ping: [yellow]{p}[/yellow] мс"
-        elif p < 60: p = f"Ping: [bold green]{p}[/bold green] мс"
-    except:
-        p = f"Ping: [bold red]Сбой[/bold red]"
-
-    console2.print('\n', Panel.fit(f"{d}\n{u}\n{p}\n\nВаш ip: {v4.get('ip')}\nПровайдер: {v4.get('isp')}\nЛокация: {v4.get('country')}",
-                                   title="🌐 Тест сети:", style=STL(color="cyan")))
+# Скорость выгрузки.
+        try:
+            if u < 0.8: u = f"Upload: [bold red]{u}[/bold red] Мбит/с"
+            elif 0.8 <= u <= 1.5: u = f"Upload: [yellow]{u}[/yellow] Мбит/с"
+            elif u > 1.5: u = f"Upload: [bold green]{u}[/bold green] Мбит/с"
+        except:
+            u = f"Upload: [bold red]Сбой[/bold red]"
+# Ping.
+        try:
+            if p >= 250: p = f"Ping: [bold red]{p}[/bold red] мс"
+            elif 60 <= p < 250: p = f"Ping: [yellow]{p}[/yellow] мс"
+            elif p < 60: p = f"Ping: [bold green]{p}[/bold green] мс"
+        except:
+            p = f"Ping: [bold red]Сбой[/bold red]"
+# Результат.
+        console2.print(Panel.fit(f"{d}\n{u}\n{p}\n\nВаш ip: {v4.get('ip')}\nПровайдер: {v4.get('isp')}\nЛокация: {v4.get('country')}",
+                                       title="🌐 Тест сети:", style=STL(color="cyan")))
+        console2.log("[cyan]--> тест сети")
