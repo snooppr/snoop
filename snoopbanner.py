6984526f8c69285316d3c198277efc495d6ad674
snoop <61022210+snooppr@users.noreply.github.com>
2022-03-23 21:03:37 +0300

    1.5k_sites
    Former-commit-id: 4709353a03edb0bbf3f24f999aa7b93dc00c7d2e [formerly 9ba81b9aa2e2bb19e352bf95a78454af2ecd8c74]
    Former-commit-id: 2f9062e68c5cae01757325a5ee9a2bdd59021b57

--- snoopbanner.py
+++ snoopbanner.py
@@ -52,7 +52,7 @@ Snoop в исполняемом виде (build-версия) предостав
 Студентам по направлению ИБ/Криминалистика Snoop ПО full version может быть
 предоставлено на безвозмездной основе.
 
-Snoop full version: плагины без ограничений; 2400+ Websites;
+Snoop full version: плагины без ограничений; 2500+ Websites;
 поддержка и обновление Database Snoop.
 Подключение к Web_Database Snoop (online), которая расширяется/обновляется.[/bold green]
 [bold red]Ограничения demo version: Websites (Database Snoop сокращена в > 15 раз);
